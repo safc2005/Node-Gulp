6eb494416f01e0b078ac1a9d23f9a551cb7ff923
Stephen New <stevenew85@gmail.com>
2017-01-02 13:24:48 +0000

    init commit

---
+++ app/js/main.js
@@ -0,0 +1,27 @@
+// self invoking funciton keeping scope contained and not global
+
+(function() {
+
+  var safr = {
+    // caching the DOM elements that are needed thtoughout the process
+    cacheDom: function () {
+      this.$mEl = document.getElementById('main');
+    },
+    // initilised the entire project
+    init: function () {
+      this.cacheDom();
+      this.render();
+    },
+    // renders the DOM with the required changes
+    render: function () {
+      this.$mEl.innerHTML = this.string;
+    },
+    string: 'Stephen'
+  };
+
+  // awaits for the DOM to be loaded then calls the init module
+  document.addEventListener("DOMContentLoaded", function(event) {
+    safr.init();
+  });
+
+})();
